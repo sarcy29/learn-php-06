610e0ed5955c174c66348076cf601bf38e1317cf
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-04-09 10:23:39 +0900

    add php 06

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sarcy Nenepat</title>
+</head>
+
+<body>
+    <?php
+        echo str_word_count("Hi Sarcy");
+    ?>
+</body>
+</html>
